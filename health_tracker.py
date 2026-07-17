99907ab7ad993a00d34df23ff2e14a01f0ac1bc4
zayedkhan1 <zayedk458@gmail.com>
2026-07-17 21:16:31 +0600

    add and get record functionality added

--- health_tracker.py
+++ health_tracker.py
@@ -1,21 +1,44 @@
 import numpy as np
+import json
+import os
+import datetime
+from record import HealthRecord
 
 class HealthTracker:
 
     def __init__(self,filename='health_tracker.py'):
-        pass
+        self.filename=filename
+        self.records=[]
 
 
-    def add_record():
-        pass
 
-    def get_record():
-        pass
+    def add_record(self,record):
+        for old_record in self.records:
+            if old_record.date == record.date:
+                self.records.remove(old_record)
+                self.records.append(record)
+                self.save_data()
+                return
+
+        self.records.append(record)
+        self.save_data()
+
+    def get_record(self,date):
+        for record in self.records:
+            if record.date==date:
+                return record
+            else:
+                return None
+        return None    
+              
     
     def delete_record():
         pass
 
-    def get_al_record():
+    def get_all_record():
         pass
 
+
+    def save_data():
+        pass
     
